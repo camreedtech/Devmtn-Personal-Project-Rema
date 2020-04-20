51a2b5440bd979f18a24d34f060844c5d5551213
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-20 10:21:54 -0700

    added dummy data to db

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,55 @@
-Put dummy data in here
+DROP TABLE IF EXISTS study;
+DROP TABLE IF EXISTS words;
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username TEXT,
+  hashed_password TEXT,
+  email VARCHAR(32)
+);
+
+INSERT INTO users (username, email)
+VALUES
+('Socrates', 'WhatIsTruth?@Athens.edu'),
+('Plato', 'TheRepublic@TheCave.org'),
+('Aristotle', 'PhilosophyIzKool@VirtueEthics.com')
+
+CREATE TABLE study (
+  word_id SERIAL PRIMARY KEY,
+  english TEXT,
+  greek TEXT
+);
+
+INSERT INTO study (english_word, greek_word)
+VALUES
+()
+
+CREATE TABLE words (
+  word_id SERIAL PRIMARY KEY,
+  english TEXT,
+  greek TEXT
+); 
+
+INSERT INTO words (english_word, greek_word)
+VALUES
+('area', 'περιοχή'),
+('book', 'βίβλιο'),
+('philosophy', 'φιλοσοφία'),
+('ethics', 'ήθικη'),
+('country', 'κράτος'),
+('family', 'οικογένεια'),
+('government', 'κυβέρνυση'),
+('mother', 'μητέρα'),
+('father', 'πατέρα'),
+('child', 'παιδί'),
+('I think', 'σκέφτομαι'),
+('I eat', 'τρώω'),
+('I go', 'πηγαίνω'),
+('I try', 'προσπαθώ'),
+('I speak', 'μιλάω')
+('I laugh', 'γελάω')
+('I leave', 'φεύγω')
+('I see', 'βλέπω')
+('I hear', 'ακούω')
+('I smell', 'μυρίζω')
