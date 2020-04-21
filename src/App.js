b07f94a956125e7754fb261a1e0a98b1d7676e2f
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-21 11:34:30 -0700

    moved routes from index.js to app.js in src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { }
-import './App.css';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import store from './Redux/store';
+import Signin from './Signin';
+import Register from './Register';
+import Home from './Home';
+import Study from './Study';
+import Vocab from './Vocab';
 
 function App() {
   return (
@@ -12,3 +20,24 @@ function App() {
 }
 
 export default App;
+
+
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Landing}/>
+      <Route path="/Signin" component={Signin}/>
+      <Route path="/Register" component={Register}/>
+      <Route path="/Home" component={Home}/>
+      <Route path="/Study" component={Study}/>
+      <Route path="/Vocab" component={Vocab}/>
+        
+    </Switch>
+  </Router>
+
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
