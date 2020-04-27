daa2ab7dbfcf8082fa90731ddf223eafa2b5cd60
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-27 10:23:05 -0700

    made a couple redux files, still need to complete it

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import {createStore, applyMiddleware} from 'redux'
+
+import promiseMiddleware from 'redux-promise-middleware'
+
+import reducer from './reducers/index'
+
+export default createStore(reducer, applyMiddleware(promisedMiddleware))
