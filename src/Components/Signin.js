6550a7a9330fa20554d3fc341a67e02c2de7abab
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-27 14:59:46 -0700

    Coded signin

---
+++ src/Components/Signin.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {signin} from '../../redux/reducers/index.js';
+
+
+class Signin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+        };
+        this.signinHandler = this.signinHandler.bind(this)
+        this.changeHandler = this.changeHandler.bind(this)
+    }
+    signinHandler(e) {
+        e.preventDefault();
+        this.props
+            .signin(this.state)
+            .then(() => {
+                this.props.redirect();
+            })
+            .catch((err) => console.log('Error signing in', err));
+    }
+    changeHandler(e) {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+    render() {
+        return(
+            <div className='Signin-container'>
+                <form onSubmit={this.signinHandler}>
+                    <input
+                        placeholder="password"
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        onChange={(e) => this.changeHandler(e)}
+                    />
+                    <button className='signin-button'>Sign In</button>
+                </form>
+                {this.props.location ? null : 
+                <button className='click-register-button' onClick={this.props.display}>Click to Register</button>
+                }
+            </div>
+        );
+    }
+}
+
+export default connect(null, {signin})(Signin);
