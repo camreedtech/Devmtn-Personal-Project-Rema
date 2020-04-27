8b6f4efbef3c3754b29b34431503d069c431406b
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-27 10:18:52 -0700

    updated readme file, added join to db but still need to finish

--- db/init.sql
+++ db/init.sql
@@ -15,12 +15,20 @@ VALUES
 ('Plato', 'TheRepublic@TheCave.org'),
 ('Aristotle', 'PhilosophyIzKool@VirtueEthics.com');
 
+SELECT english_word AND greek_word FROM words
+JOIN ON STUDY WHERE  s.word_id w.word_id 
+WHERE user_id = $1
+
+--have endpoint that runs study query 
+-- insert word id(comes form front end) and user id (comes from req.session) into study table 
+
 CREATE TABLE study (
-  word_id SERIAL PRIMARY KEY,
-  english_word TEXT,
-  greek_word TEXT,
+  study_id SERIAL PRIMARY KEY,
+  word_id INT REFERENCES words(word_id),
   user_id INT REFERENCES users(user_id)
 );
+
+
 
 CREATE TABLE words (
   word_id SERIAL PRIMARY KEY,
