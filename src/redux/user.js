4048a99e06b2e4291d80cb101b5011623f42b13e
Cameron Reed <cameronreedadjusting@gmail.com>
2020-04-28 10:33:00 -0700

    wrote user.js register, signin, signout

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,1 +1,97 @@
-import 
+import axios from 'axios';
+
+const REGISTER_USER ='REGISTER_USER'
+const SIGNIN_USER = 'SIGNIN_USER'
+const SIGNOUT_USER = 'SIGNOUT_USER'
+
+const initialState = {
+    data: null,
+    loading: false
+}
+
+export default function(state = initialState, action) {
+    let {type, payload} = action
+
+    switch(type) {
+        case REGISTER_USER + '_PENDING':
+            return {
+                ...state,
+                loading: true 
+            }
+        
+        case REGISTER_USER = '_FULFILLED':
+            return {
+                ...state, 
+                data: payload.data,
+                loading: false
+            }
+        
+        case REGISTER_USER + '_REJECTED':
+            return {
+                ...state, 
+                loading: false 
+            }
+        
+        case SIGNIN_USER + '_PENDING':
+            return {
+                ...state, 
+                loading: true 
+            }
+
+        case SIGNIN_USER + '_FULFILLED':
+            return {
+                ...state, 
+                data: payload.data,
+                loading: false 
+            }
+
+        case SIGNIN_USER + '_REJECTED':
+            return {
+                ...state, 
+                loading: false 
+            }    
+
+        case SIGNOUT_USER + '_PENDING':
+            return { 
+                ...state, 
+                loading: true 
+            }
+
+        case SIGNOUT_USER + '_FULFILLED':
+            return {
+                ...state, 
+                data: null,
+                loading: false 
+            }
+
+        case SIGNOUT_USER + '_REJECTED':
+            return {
+                ...state, 
+                loading: false
+            }
+
+        default: 
+            return state
+    }
+}
+
+export function register(userInfo) {
+    return {
+        type: REGISTER_USER,
+        payload: axios.post('/auth/register', userInfo)
+    }
+}
+
+export function signin(userInfo) {
+    return {
+        type: SIGNIN_USER,
+        payload: axios.post('/auth/login', userInfo)
+    }
+}
+
+export function signout() {
+    return {
+        type: SIGNOUT_USER,
+        payload: axios.delete('/auth/signout')
+    }
+}
